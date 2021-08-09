bd674b54001ae0dd49c280d0022dd14b12424206
li_lubo <li_lubo@aliyun.com>
2021-08-09 15:20:20 +0800

    bytes.join()方法示例

--- Demo/jsonDemo.go
+++ Demo/jsonDemo.go
@@ -1,14 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"fmt"
 	"strings"
 )
 
 func main() {
-	strs := []string{"hello", "world", "itcast"}
+	strs := []string{"hello", "world", "lubo"}
 
 	str := strings.Join(strs, "=")
 
 	fmt.Printf("str: %s\n", str)
+
+	joinRes := bytes.Join([][]byte{[]byte("hello"), []byte("world"), []byte("lubo")}, []byte("="))
+	fmt.Printf("joinRes: %s\n", joinRes)
 }
