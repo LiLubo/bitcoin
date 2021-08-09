a49bcf9132273a7c5304a535783a506f27ace05d
li_lubo <li_lubo@aliyun.com>
2021-08-09 21:08:02 +0800

    Run函数的实现，用于不断的计算哈希并比较

--- ProofOfWork.go
+++ ProofOfWork.go
@@ -1,6 +1,9 @@
 package main
 
 import (
+	"bytes"
+	"crypto/sha256"
+	"fmt"
 	"math/big"
 )
 
@@ -33,4 +36,68 @@ func NewProofOfWork(block *Block) *ProofOfWork {
 	pow.target = &bigIntTmp
 
 	return &pow
+}
+
+// 3.不断计算 hash 的函数, 获取挖矿的 Nonce,同时返回区块的哈希
+func (pow *ProofOfWork) Run() ([]byte, uint64) {
+	// 声明随机变量
+	var nonce uint64
+	// 声明哈希数组
+	var hash [32]byte
+
+	for {
+		// 拼接 Block 和 Nonce
+		data := pow.prepareData(nonce)
+
+		// c.sha256
+		hash /* 数组类型 */ = sha256.Sum256(data)
+
+		// d.与难度值比较
+		// 将 hash(数组类型) 转成 big.int,然后与 pow.target 进行比较，需要引入局部变量
+		var bigIntTmp big.Int
+		bigIntTmp.SetBytes(hash[:])
+
+		// 比较
+		/*
+			func (x *Int) Cmp(y *Int) (r int)
+			Cmp compares x and y and returns:
+			-1 if x <  y
+			0  if x == y
+			+1 if x >  y
+
+		*/
+		result := bigIntTmp.Cmp(pow.target)
+
+		if result == -1 {
+			// Ⅰ：若哈希值小于难度值，挖矿成功，退出
+			fmt.Printf("挖矿成功！Nonce: %v, hash: %v\n", nonce, hash)
+			break
+		} else {
+			// Ⅱ: 若哈希值大于难度值，nonce ++
+			nonce++
+		}
+	}
+
+	return hash[:], nonce
+}
+
+// 4. 提供一个校验函数
+//     - `IsValid()`
+
+func (pow *ProofOfWork) prepareData(nonce uint64) []byte {
+	// a.获取 block 数据
+	block := pow.block
+
+	// b.拼接 nonce
+	tmp := [][]byte{
+		uintToByte(block.Version),
+		block.PrevBlockHash,
+		block.MerKleRoot,
+		uintToByte(block.TimeStamp),
+		uintToByte(block.Difficulity),
+		block.Data,
+		uintToByte(nonce),
+	}
+
+	return bytes.Join(tmp, []byte{})
 }
